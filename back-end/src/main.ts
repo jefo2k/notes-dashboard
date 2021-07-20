32efa2fe74e725f0ba20c8244704e8575fe88784
Jeferson Santos <jefo2k@gmail.com>
2021-07-20 18:11:53 -0300

    add helmet

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
+
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Notes Board API')
